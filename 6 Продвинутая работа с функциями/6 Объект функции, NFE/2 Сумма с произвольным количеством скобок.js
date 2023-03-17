ef72d9510f4c116c5008d40704b107198cfe0ed0
linabaris <lina.fakhrutdinova@gmail.com>
2023-03-17 17:17:05 +0300

    finished tasks in advansed func section

--- 6 Продвинутая работа с функциями/6 Объект функции, NFE/2 Сумма с произвольным количеством скобок.js
+++ 6 Продвинутая работа с функциями/6 Объект функции, NFE/2 Сумма с произвольным количеством скобок.js
@@ -1,4 +1,12 @@
 //Сумма с произвольным количеством скобок
 let sum = function func(a) {
-    
+    let currentSum = a;
+    function f(b) {
+        currentSum +=b;
+        return f;
+    }
+    f.toString = function() {
+        return currentSum;
+    }
+    return f;
 }
--- 6 Продвинутая работа с функциями/9 Декораторы и переадресация вызова, call apply/3 Декоратор debounce.js
+++ 6 Продвинутая работа с функциями/9 Декораторы и переадресация вызова, call apply/3 Декоратор debounce.js
@@ -1,3 +1,9 @@
 function debounce(f, ms) {
-    
+    let state = false;
+    return function() {
+        if(state) return;
+        f.apply(this,arguments);
+        state=true;
+        setTimeout(() => state = false,ms)
+    }
 }
